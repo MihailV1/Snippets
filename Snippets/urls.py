bca5c6424163ffd19742cc278036026fde1e2815
Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>
2025-09-08 22:01:02 +0200

    fix urls for Render.com

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views, views_cbv
 from django.contrib import admin
-from debug_toolbar.toolbar import debug_toolbar_urls
+
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
@@ -26,7 +27,7 @@ urlpatterns = [
                   # path('logout/', views.user_logout, name='logout'),
                   path('logout/', views_cbv.UserLogoutView.as_view(), name='logout'),
                   # path('registration/', views.user_registration, name='registration'),
-path('registration/', views_cbv.UserRegistrationView.as_view(), name='registration'),
+                  path('registration/', views_cbv.UserRegistrationView.as_view(), name='registration'),
                   path('comment/add', views.comment_add, name="comment_add"),
                   path('stats/', views.stats_snippets, name="stats_snippets"),
                   # path('notifications/', views.user_notifications, name="notifications"),
@@ -39,11 +40,11 @@ path('registration/', views_cbv.UserRegistrationView.as_view(), name='registrati
                   # path('api/comment/<int:id>/<str:str>', views.like_dislike, name='like_dislike'),
                   path('api/comment/vote/', views.comment_like_dislike, name='comment_like_dislike'),
                   path('api/snippet/vote/', views.snippet_like_dislike, name='snippet_like_dislike'),
-path('snippet/<int:id>/subscribe/', views.subscribe_to_snippet, name='subscribe-to-snippet'),
+                  path('snippet/<int:id>/subscribe/', views.subscribe_to_snippet, name='subscribe-to-snippet'),
                   path('profile/', views.user_profile, name="user_profile"),
                   path('profile/edit', views.edit_profile, name="user_profile_edit"),
                   path('profile/edit', views.edit_profile, name="user_profile_edit"),
-              ] + debug_toolbar_urls()
+              ] #+ debug_toolbar_urls()
 
 if settings.DEBUG:
     # Добавляем debug_toolbar URLs только в режиме разработки
