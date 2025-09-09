e9fab5fba0f8005ca88e9fe2614299b290f20ec3
Mykhailo Vyshnevskyi <vishnevskiy.mihail@gmail.com>
2025-09-09 23:27:31 +0200

    Add create_superuser to django for Render.com

--- build.sh
+++ build.sh
@@ -15,3 +15,21 @@ echo "Количество файлов: $(find staticfiles/ -type f | wc -l)"
 
 # Применение миграций
 python manage.py migrate
+
+# === Создание суперпользователя, если он ещё не создан ===
+if [[ -n "$CREATE_SUPERUSER" ]]; then
+  echo "Проверка существования суперпользователя..."
+  python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "$DJANGO_SUPERUSER_USERNAME"
+email = "$DJANGO_SUPERUSER_EMAIL"
+password = "$DJANGO_SUPERUSER_PASSWORD"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Суперпользователь создан:", username)
+else:
+    print("Суперпользователь уже существует:", username)
+EOF
+fi
